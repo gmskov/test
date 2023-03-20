d2c11098c588992b28b5d3737d1350a169326b62
Andrey Kovalenko <gms.kov@gmail.com>
2023-03-20 17:39:40 +0200

    qwe

---
+++ erp-ui/deploy.js
@@ -0,0 +1,13 @@
+var sftp = require('node-sftp-deploy');
+sftp({
+  "host": "167.71.39.235",
+  "port": "22",
+  "user": "gbar",
+  "pass": "123QweAs",
+  "remotePath": "./gbar.digitize.ee/test",
+  "sourcePath": "./public"
+}, function(){
+  console.log('good')
+});
+
+
